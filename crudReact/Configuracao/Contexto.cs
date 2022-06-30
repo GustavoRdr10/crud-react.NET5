17907f20d15f0ca9e3a1e5c0d985e73fb5f1563e
Gustavo Rodrigues <gustavo.rodrigues@sequor.com.br>
2022-06-30 15:24:38 -0300

    feat: context_controller

---
+++ crudReact/Configuracao/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using crudReact.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudReact.Configuracao {
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
