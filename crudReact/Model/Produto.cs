9f57ca12849205f0ff2f8fd3c7a6e350f6f9be8b
Gustavo Rodrigues <gustavo.rodrigues@sequor.com.br>
2022-06-30 14:37:14 -0300

    feat:criando-modal

---
+++ crudReact/Model/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace crudReact.Model {
+
+    [Table("Produto")]
+    public class Produto 
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Descricao")]
+        public string Descricao { get; set; }
+
+
+    }
+}
